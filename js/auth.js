3b3f377f9234880cca088d85b237e628ca2b86e3
luongminhthevinh <thevinh16062003@gmail.com>
2022-11-03 15:51:23 +0700

    Add new project food

---
+++ js/auth.js
@@ -0,0 +1,52 @@
+const userLISTSTR = localStorage.getItem("users")
+const users = JSON.parse(userLISTSTR ?? "[]")
+const nguoidunght = localStorage.getItem("nguoidungdangdn") ? JSON.parse(localStorage.getItem("nguoidungdangdn")) : null
+if (nguoidunght) {
+    document.getElementById("user").innerHTML = `${nguoidunght.name}`
+} else {
+    let loginForm = document.querySelector('.login-form');
+
+    document.querySelector('#login-btn').onclick = () => {
+        loginForm.classList.toggle('active');
+        signUp.classList.remove('active');
+        searchForm.classList.remove('active');
+        shoppingCart.classList.remove('active');
+        navbar.classList.remove('active');
+    }
+}
+
+function onSignUp() {
+    const emailElement = document.querySelector("#su_email");
+    const nameElement = document.querySelector("#su_name");
+    const passwordElement = document.querySelector("#su_password");
+    const emailPert = /^[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*$/gm;
+    if (!emailElement.value.match(emailPert)) {
+        return
+    }
+    const exituser = users.filter(u => u.email = emailElement.value);
+    if (exituser[0]) {
+        return
+    }
+    const user = { name: nameElement.value, email: emailElement.value, password: passwordElement.value }
+    users.push(user)
+    alert(user);
+    localStorage.setItem("users", JSON.stringify(users));
+}
+
+
+function onLogin() {
+    alert("ok")
+    const emailElement = document.querySelector("#lg_email");
+    const passwordElement = document.querySelector("#lg_password");
+    const exituser = users.filter(u => u.email == emailElement.value);
+    console.log(users);
+    if (!exituser[0]) {
+        return // sai email
+    }
+    if (!exituser[0].password == passwordElement.value) {
+        return // mk k dung
+    }
+    alert(exituser[0])
+    localStorage.setItem("nguoidungdangdn", JSON.stringify(exituser[0]));
+    location.reload();
+}
---
+++ js/script.js
@@ -0,0 +1,90 @@
+let searchForm = document.querySelector('.search-form');
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+    signUp.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let shoppingCart = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart-btn').onclick = () => {
+    shoppingCart.classList.toggle('active');
+    signUp.classList.remove('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let signUp = document.querySelector('.sign-up');
+
+document.querySelector('#open-sign-up').onclick = () => {
+    signUp.classList.toggle('active');
+    loginForm.classList.remove('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+    signUp.classList.remove('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+window.onscroll = () => {
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    signUp.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+var swiper = new Swiper(".product-slider", {
+    loop: true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    centeredSlides: true,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1020: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+var swiper = new Swiper(".review-slider", {
+    loop: true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    centeredSlides: true,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1020: {
+            slidesPerView: 3,
+        },
+    },
+}); 
